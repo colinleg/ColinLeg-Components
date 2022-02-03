9a1818065dba66ead22ad549c0a848607b2b0634
Colin Legoedec <clegoedec@gmail.com>
2022-02-04 00:51:11 +0100

    initial commit

---
+++ sfc/dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import ColinlegInput from '@/colinlegInput.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    ColinlegInput
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <colinlegInput />
+  </div>
+</template>
---
+++ sfc/src/colinlegInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <input ref="input" v-model="input" @blur="restart" type="text" placeholder="my-component" class="myInput">
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default /*#__PURE__*/defineComponent({
+  name: 'ColinlegInput', // vue component name
+  data(){
+    return {
+      input: null
+    }
+  },
+  methods: {
+    restart() {
+      this.$refs.input.value = ""
+    }
+  }
+});
+</script>
+
+
+
+<style scoped>
+  .colinlegInput {
+    display: block;
+    width: 400px;
+    margin: 25px auto;
+    border: 1px solid #ccc;
+    background: #eaeaea;
+    text-align: center;
+    padding: 25px;
+  }
+  .colinlegInput p {
+    margin: 0 0 1em;
+  }
+
+  .myInput {
+    border: 5px solid #4893bd;
+    border-radius: 3px;
+    box-shadow: #9c8f8f 3px 3px;
+    outline: none;
+    padding: 10px;
+  }
+</style>
